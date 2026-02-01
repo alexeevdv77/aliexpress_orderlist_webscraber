3daabd01d199ed36da5a1a39e29d2df835245573
Dmitry Alexeev <alexeevd@rambler.ru>
2026-02-01 17:25:04 +0300

    aliexpress.ru mod

--- get_cookies.py
+++ get_cookies.py
@@ -5,7 +5,8 @@ browser = webdriver.Firefox()
 
 
 def get_cookies():
-    browser.get("https://login.aliexpress.com/buyer.htm?return=https%3A%2F%2Fwww.aliexpress.com%2F&random=CEA73DF4D81D4775227F78080B9B6126")
+    #browser.get("https://login.aliexpress.com/buyer.htm?return=https%3A%2F%2Fwww.aliexpress.com%2F&random=CEA73DF4D81D4775227F78080B9B6126")
+    browser.get("https://aliexpress.ru/account?spm=a2g2w.home.profil.1.5a505586y4WLcS")	
     print('input your username and password in Firefox and hit Submit')
     input('Hit Enter here if you have summited the form: <Enter>')
     cookies = browser.get_cookies()
@@ -14,12 +15,13 @@ def get_cookies():
 
 
 def set_cookies():
-    browser.get("https://aliexpress.com")
+    browser.get("https://aliexpress.ru")
     cookies = pickle.load(open("cookies.pickle", "rb"))
     for cookie in cookies:
         browser.add_cookie(cookie)
-    browser.get("https://bestselling.aliexpress.com/en")
+    browser.get("https://aliexpress.ru/one-price")
 
 
 if __name__ == '__main__':
     get_cookies()
+    #set_cookies()
--- order_list.py
+++ order_list.py
@@ -1,27 +1,40 @@
+# Экспортрует архивные заказы из aliexpress.ru с использованием управления браузером FireFox
+# Проверялось на python 3.10, убедитесь, что используемые библиотеки установлены
+# Сначала получите кук - get_cookies.py, логинитесь (может падать!) -> cookies.pickle
+# кук работает какое-то время, потом надо новый получать
+# Скрипт завязан на разметку, особо опасные места помечены !!
+# Количество заказов - сколько отражается, можно полистать - см константу NUM_SCROLL
+# Трэк, полученно/не получено здесь не анализируется - можно доделать
+# Если что-то изменится в разметке или новое понадобится - см. режим разработчика в браузере и доку на selenium
+# Если года в дате заказа нет, то он ниже устанавливается в 2026
+# Переноса по словам в многосточных ясейках нет - можно настроить уже в excell
+# Повторно заказы не эксортируются - см saved_orders.json, можно экспртировать то, чего еще не было
+# для того чтобы не банили имеется задержка sleep
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import pickle
 import time
+import json
 
-import sys
+import os
 # Import the byte stream handler.
 from io import BytesIO
-# Import urlopen() for either Python 2 or 3.
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
-
 
 import xlsxwriter
 
+FN_SAVE = 'saved_orders.json'
 
+NUM_SCROLL = 1
+KEYS_LIST = ['IMAGE','NAME','PRICE','QTY','DATE','STORE','ORDER NO']
 
-
-NUM_OF_PAGES_TO_SEARCH = 1
-KEYS_LIST = ['IMAGE','NAME','ORDER NO','TRACK NO','POU','DELIVERY']
+saved_orders = []
+if os.path.exists(FN_SAVE):
+    with open(FN_SAVE, 'r') as json_file:  
+        saved_orders = json.load(json_file)  
 
 driver = webdriver.Firefox()
-driver.get("https://aliexpress.com")
+driver.get("https://aliexpress.ru")
 cookies = pickle.load(open("cookies.pickle", "rb"))
 for cookie in cookies:
     driver.add_cookie(cookie)
@@ -41,13 +54,13 @@ def write_to_xlsx_file(file_name,list_dict_items):
     row_index = 1
 
     workbook = xlsxwriter.Workbook(file_name)
-    workbook.formats[0].set_font_size(20)
+    workbook.formats[0].set_font_size(12)
     worksheet = workbook.add_worksheet()
 
-    for col in range(0,100):
+    for col in range(0,500):
         # worksheet.set_column(col, col, 70)
         worksheet.set_column(col,col, 15)
-        worksheet.set_row(col, 80) 
+        worksheet.set_row(col, 66) 
 
 
 
@@ -67,139 +80,116 @@ def write_to_xlsx_file(file_name,list_dict_items):
             element_pos = str(list_of_columns[indx])+str(row_index)
             if key == KEYS_LIST[0]:
                 # Read an image from a remote url.
-                url = value
-                image_data = BytesIO(urlopen(url).read())
+                #url = value
+                image_data = BytesIO(value)
+                print(f'Set img [{len(value)}] to {element_pos}')
                 # Write the byte stream image to a cell. Note, the filename must be
                 # specified. In this case it will be read from url string.
-                worksheet.insert_image(element_pos, url, {'image_data': image_data,'x_scale': 1.5, 'y_scale': 1.5,'x_offset': 7, 'y_offset': 7})
+                try:
+                    worksheet.insert_image(element_pos, 'img.png', {'image_data': image_data, 'x_offset': 7, 'y_offset': 7})
+                except Exception as e:
+                    print(e)
+                    pass
             else:
+                value = value.replace('\n', '\r\n')
                 worksheet.write(element_pos, value)
     workbook.close()
 
 
-def get_item_details(url):
+def get_item_details(url):    
+    print(f'Getting {url} ...')
+    res = []
     driver.get(url)
-    item_name_tab            = driver.find_elements_by_css_selector('a.baobei-name')
-    item_order_num_tab       = driver.find_elements_by_css_selector('dd.order-no')
-    item_tracking_num_tab    = driver.find_elements_by_css_selector('td.no')
-    ariel_order_num_tab      = driver.find_elements_by_css_selector('span.i18ncopy')
-    item_delivery_status_tab = driver.find_elements_by_css_selector('div.list-box')
-    item_image_url           = driver.find_elements_by_css_selector('a.pic')[0].find_elements_by_tag_name('img')[0].get_attribute('src')
+    order_num = 'unknown'
+    store = 'unknwon'
+    order_date = 'unknwon'
+    el_order_num = driver.find_elements(By.CSS_SELECTOR, "[data-testid='orderNumber']")    
+    if el_order_num:
+        order_num = el_order_num[0].text
+        info: str = el_order_num[0].find_element(By.XPATH, '..').find_element(By.XPATH, '..').text
+        i = info.find("от ")  # from Date
+        if i > 0:
+            order_date = info[i+3:].strip()
+            if not '202' in order_date:
+                order_date += ' 2026'  # current year
 
-    row_dict = dict()
-    key_index = 0
+    print(f'Order {order_num}')    
+    els_seller = driver.find_elements(By.CSS_SELECTOR, "[data-testid='sellerInfoV2']") 
+    if els_seller:
+        store = els_seller[0].text + '\n' + els_seller[0].get_attribute('href')
 
-    row_dict[KEYS_LIST[key_index]]=item_image_url
-    key_index += 1
+    els_item_img = driver.find_elements(By.CSS_SELECTOR, "[data-testid='product']")    
+    item_imgs = []
+    for el in els_item_img:
+        item_imgs.append(el.find_element(By.TAG_NAME, 'picture').find_element(By.TAG_NAME, 'img'))    
 
-    for name in item_name_tab:
-        row_dict[KEYS_LIST[key_index]]=name.text.replace('\n', ':').replace(',', ':')
-        key_index += 1
-        # print('NAME : ',name.text)
-        break
-
-    for order_no in item_order_num_tab:
-        row_dict[KEYS_LIST[key_index]]=order_no.text.replace('\n', ':').replace(',', ':')
-        key_index += 1
-        # print('ORDER NO : ',order_no.text)
-        break
-
-    for item_tracking in item_tracking_num_tab:
-        row_dict[KEYS_LIST[key_index]]=item_tracking.text.replace('\n', ':').replace(',', ':')
-        key_index += 1
-        # print('TRACK NO : ',item_tracking.text)
-        break
-
-    for ariel_order in ariel_order_num_tab:
-        if 'POU' in ariel_order.text and len(ariel_order.text) < 15:
-            row_dict[KEYS_LIST[key_index]]=ariel_order.text.replace('\n', ':').replace(',', ':')
-            key_index += 1
-            # print('ARIEL NO : ',ariel_order.text)
-            break
-
-    for indx,item_delivery_status in enumerate(item_delivery_status_tab):
-        row_dict[KEYS_LIST[key_index]]=item_delivery_status.text.replace('\n', ':').replace(',', ':')
-        key_index += 1
-        # print('DELIVERY : ',item_delivery_status.text)
-        break
-    #print('---------------------------------------------------------')
-    return row_dict
+    els_item_name = driver.find_elements(By.CSS_SELECTOR, "[data-testid='productText']")
+    if not els_item_name:
+        return res
+    
+    for i, el_name in enumerate(els_item_name):
+        
+        row_dict = dict()        
+        row_dict['IMAGE'] = item_imgs[i].screenshot_as_png
+        el_parent = el_name.find_element(By.XPATH, '..')
+        # product name + product type + url
+        row_dict['NAME'] = el_parent.text + '\n' + el_name.get_attribute('href')
+        # !!!
+        els_price = el_parent.find_element(By.XPATH, '..').find_elements(By.CSS_SELECTOR, 'div.RedOrderDetailsProductsV2_Product__priceDesktop__1nlc3')
+        if els_price:
+            s = els_price[0].text.split('\n')
+            row_dict['PRICE'] = s[0].strip()
+            if len(s) > 1:
+                row_dict['QTY'] = s[1].strip()
+        row_dict['DATE'] = order_date
+        row_dict['STORE'] = store
+        row_dict['ORDER NO'] = order_num
+           
+        res.append(row_dict)
+        #print(row_dict)
+    
+    return res
 
 
 def get_list_of_item_view_details(url):
+    print(f'Getting {url} ...')
     driver.get(url)
     links = set()
     items_dict_list = []
-
-    for i in range(NUM_OF_PAGES_TO_SEARCH):
-        view_details = driver.find_elements_by_css_selector('a.view-detail-link')
-        # Fill list of links to check
-        for indx,view in enumerate(view_details):
-            link = view.get_attribute('href')
-            links.add(link)
-        # Move to next page
-        next_tab = driver.find_elements_by_css_selector('a.ui-pagination-next')
-        for n in next_tab:
-            n.click()
-            break
+    print(f'Scroll down by {NUM_SCROLL} pages...')
+    for i in range(NUM_SCROLL):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(5)  # time for loading
+    print(f'Collecting links...')
+    view_details = driver.find_elements(By.CSS_SELECTOR, 'a.RedOrderList_OrderItem__link__1tjf5') # !!!
+    # Fill list of links to check
+    for indx,view in enumerate(view_details):
+        link = view.get_attribute('href')
+        links.add(link)
     
-
+    print(f'{len(links)} links collected')
+    links = sorted(links, reverse=True)     # Most recent first
 
     # Get dict representing a single ROW
     for link in links:
-        row_dict = get_item_details(link)
-        items_dict_list.append(row_dict)
+        p = link.split('/')     #https://aliexpress.ru/order-list/<number>?filterName=archive
+        s = p[-1]
+        p = s.split('?')
+        order_id = p[0]
+        if order_id in saved_orders:    # exported allready?
+            continue
+        time.sleep(3)  # prevent ddos filter
+        items = get_item_details(link)
+        items_dict_list.extend(items)
+        saved_orders.append(order_id)
 
     driver.close()
     return items_dict_list
 
-    # Open CSV file
-    # file = open('aliexpress_oreders.csv','w')
-    # for col in KEYS_LIST:
-    #     file.write(col+',')
-    # file.write('\n')
-    
-    #     for key in KEYS_LIST:
-    #         value=''
-    #         try:
-    #             value = row_dict[key]
-    #         except Exception:
-    #             pass
-    #         file.write(value+',')
-    #     file.write('\n')
-    
-    # file.close() 
-    
-
-
-
-# def extract_product_urls_from_list_page(list_page_url):
-#     driver.get(list_page_url)
-#     time.sleep(5)
-#     cats = driver.find_elements_by_css_selector('a.baobei-name')
-
-#     all_links = set()
-#     for ind, cat in enumerate(cats):
-#         print(cat.text)
-#         # try:
-#         #     cat.click()
-#         # except Exception:
-#         #     continue
-#         # if ind == 0:
-#         #     items = driver.find_elements_by_class_name('item-desc')
-#         #     links = [item.get_attribute('href') for item in items]
-#         # else:
-#         #     items = driver.find_elements_by_css_selector('div.title > a')
-#         #     links = [item.get_attribute('href') for item in items]
-#         # for link in links:
-#         #     all_links.add(link)
-#         # time.sleep(2)
-#     return all_links
-
 
 if __name__ == '__main__':
-    # extract_product_urls_from_list_page('https://trade.aliexpress.com/orderList.htm?')
-    items_dict_list = get_list_of_item_view_details('https://trade.aliexpress.com/orderList.htm')
-    write_to_xlsx_file('aliexpress_oreders.xlsx',items_dict_list)
-    #print(create_worksheet_columns(len(KEYS_LIST)))
+    items_dict_list = get_list_of_item_view_details('https://aliexpress.ru/order-list?filterName=archive')
+    write_to_xlsx_file('aliexpress_orders.xlsx',items_dict_list)
+    with open(FN_SAVE, "w") as f:
+        json.dump(saved_orders, f)
 
